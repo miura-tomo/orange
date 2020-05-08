339137e511f061d3fba06fbd913eba8ba09af526
石黒　友哉 <ishiguro-tomoya@ishigurarinoMBP.lan>
2020-05-08 15:00:03 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'dc239855c97695d8c6e69b6b5986dba2c1a5093786c000964022b6a925c5f47e6a8ebfbc81f2923c31aacde34d11adeae727bd047f47c0242938208e788150bc'
